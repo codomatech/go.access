9af8ea4c4644a76194e2bd96ed27fcc9f58dedbd
Mohamed Abdel Maksoud <mohamed.amaksoud@gmail.com>
2020-11-08 09:21:35 +0100

    Implement analysis for site visits and hits

--- goaccess/analyzers/visits/visits.go
+++ goaccess/analyzers/visits/visits.go
@@ -1,11 +1,37 @@
+/**
+ * Analysis for visits and hits
+ */
+
 package goaccess_analyzers
 
 import (
-
 	"github.com/codomatech/go.access/common"
 )
 
 func Analyze(records []common.AccessRecord) common.AnalysisResult {
-    var result common.AnalysisResult
-    return result
+	var result common.AnalysisResult
+
+	result.Init(len(records)/512, "visits", "hits")
+	result.Name = "Visits & Hits"
+
+	var dayvisitors map[string]bool
+	var dayhits uint
+	var curday = ""
+
+	for _, record := range records {
+		if record.Day != curday {
+			if len(curday) > 0 {
+				result.AddX(curday)
+				result.AddY("hits", float64(dayhits))
+				result.AddY("visits", float64(len(dayvisitors)))
+			}
+			curday = record.Day
+			dayhits = 0
+			dayvisitors = make(map[string]bool)
+		}
+		dayhits += 1
+		dayvisitors[record.Ip] = true
+	}
+
+	return result
 }
